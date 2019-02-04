c5e13541dbe8b769303d3d2dbdcf22a7fa03dee9
yijun zhang <zhangyijun0518@gmail.com>
2019-02-03 22:52:45 -0800

    Changes to support three types of GM
    Changed code about the definition of GumballMachines to support three models in requests.

--- lab1/gumball-java-typical/GumballMachine.java
+++ lab1/gumball-java-typical/GumballMachine.java
@@ -1,43 +1,57 @@
-
 public class GumballMachine
 {
 
     private int num_gumballs;
-    private boolean has_quarter;
+    private int cost_gumballs;
+    private boolean all_quarters;
+    private Arraylist has_coins = new ArrayList();
+    private boolean accept_coin;
 
-    public GumballMachine( int size )
+    public GumballMachine( int size, int price, boolean type )
     {
         // initialise instance variables
         this.num_gumballs = size;
-        this.has_quarter = false;
+        this.cost_gumballs = price;
+        this.accept_coin = type; 
+        this.all_quarters = true;
     }
 
-    public void insertQuarter(int coin)
-    {
-        if ( coin == 25 )
-            this.has_quarter = true ;
-        else 
-            this.has_quarter = false ;
+    public void insertCoin( int coin )
+    {   
+        has_coins.add(coin);
+        
+        if ( coin == 5 || coin == 10 ) 
+        {
+            this.all_quarters = false ;   
+        } 
     }
     
     public void turnCrank()
     {
-    	if ( this.has_quarter )
+    	if ( this.has_coins.sum() < this.cost_gumballs ) //array not empty
     	{
+    	    if(accept_coin || all_quarters)
+    	    {
     		if ( this.num_gumballs > 0 )
-    		{
+    		{      // sum of array == cost
     			this.num_gumballs-- ;
-    			this.has_quarter = false ;
-    			System.out.println( "Thanks for your quarter.  Gumball Ejected!" ) ;
+    			this.all_quarters = true ; //remove all for arrary
+    			this.has_coins.empty() ;
+    			System.out.println( "Thanks for your coin.  Gumball Ejected!" ) ;
     		}
     		else
     		{
-    			System.out.println( "No More Gumballs!  Sorry, can't return your quarter." ) ;
+    			System.out.println( "No More Gumballs!  Sorry, can't return your coin." ) ;
     		}
-    	}
+            }
+            else
+            {
+                System.out.println( "Please insert only quarters" ) ;
+            }
+        }
     	else 
     	{
-    		System.out.println( "Please insert a quarter" ) ;
+    		System.out.println( "Please insert encough coins" ) ;
     	}        
     }
 }
--- lab1/gumball-java-typical/Main.java
+++ lab1/gumball-java-typical/Main.java
@@ -3,20 +3,22 @@
 public class Main {
 
 	public static void main(String[] args) {
-		GumballMachine gumballMachine = new GumballMachine(5);
+		GumballMachine gumballMachine_1 = new GumballMachine(5,25,false);
+		GumballMachine gumballMachine_2 = new GumballMachine(5,50,false);
+                GumballMachine gumballMachine_3 = new GumballMachine(5,50,true);
+		
+                //System.out.println(gumballMachine);
 
-		System.out.println(gumballMachine);
+		gumballMachine_1.insertCoin( 25 );
+		gumballMachine_1.turnCrank();
 
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
+		//System.out.println(gumballMachine);
 
-		System.out.println(gumballMachine);
+		gumballMachine_2.insertCoin( 25 );
+		gumballMachine_2.turnCrank();
+		gumballMachine_3.insertCoin( 25 );
+		gumballMachine_3.turnCrank();
 
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-
-		System.out.println(gumballMachine);
+		//System.out.println(gumballMachine);
 	}
 }
