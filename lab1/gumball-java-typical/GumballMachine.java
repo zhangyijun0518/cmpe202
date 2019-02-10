0ea65f16b326ac010cdca7ede5a82fe84f283cf6
yijun zhang <zhangyijun0518@gmail.com>
2019-02-09 22:02:09 -0800

    small changes

--- lab1/gumball-java-typical/GumballMachine.java
+++ lab1/gumball-java-typical/GumballMachine.java
@@ -4,8 +4,8 @@ public class GumballMachine
 
     private int num_gumballs;
     private int price_gumballs;
-    private boolean all_quarters;
-    private ArrayList<Integer> has_coins = new ArrayList<Integer>();
+    //private boolean all_quarters;
+    private int sum_coins = 0;
     private boolean accept_coin; //false if only accept quarter
 
     public GumballMachine( int size, int price, boolean type )
@@ -14,49 +14,38 @@ public class GumballMachine
         this.num_gumballs = size;
         this.price_gumballs = price;
         this.accept_coin = type; 
-        this.all_quarters = true; 
+     //   this.all_quarters = true; 
     }
 
     public void insertCoin( int coin )
     {   
-        has_coins.add(coin);
-        
-        if ( coin == 5 || coin == 10 ) 
-        {
-            this.all_quarters = false ;   
+        if (this.accept_coin || coin == 25){
+            this.sum_coins = this.sum_coins + coin;
+        } else {
+         //   this.all_quarters = false ; 
+            System.out.println("Please insert only quarters...");
         } 
     }
     
     public void turnCrank()
-    {   int sum = 0;
-        for(int i = 0; i < has_coins.size();i++){
-            sum = sum + has_coins.get(i);
-        }
-        
-    	if ( sum >= this.price_gumballs ) 
-    	{
-    	    if(accept_coin || all_quarters)
-    	    {
-    		if ( this.num_gumballs > 0 )
-    		{      
-    			this.num_gumballs-- ;
-    			this.all_quarters = true ; // reset to initial
-    			this.has_coins.clear() ;
-    			System.out.println( "Thanks for your coins.  Gumball Ejected!" ) ;
-    		}
-    		else
-    		{
-    			System.out.println( "No More Gumballs!  Sorry, can't return your coin." ) ;
-    		}
+    {   
+        if ( this.sum_coins >= this.price_gumballs ) 
+        {
+            if ( this.num_gumballs > 0 )
+            {      
+                this.num_gumballs-- ;
+              //  this.all_quarters = true ; // reset to initial
+                this.sum_coins = sum_coins - this.price_gumballs ;
+                System.out.println( "Thanks for your coins.  Gumball Ejected!" ) ;
             }
             else
             {
-                System.out.println( "Please insert only quarters" ) ;
+                System.out.println( "No More Gumballs!  Sorry, can't return your coin." ) ;
             }
         }
-    	else 
-    	{
-    		System.out.println( "Please insert encough coins" ) ;
-    	}        
+        else 
+        {
+            System.out.println( "Please insert encough coins" ) ;
+        }        
     }
 }
