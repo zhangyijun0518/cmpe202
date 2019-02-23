dd94ce0c27dbea440b775574c9132f4262f830af
yijun zhang <zhangyijun0518@gmail.com>
2019-02-23 03:39:14 -0800

    Complete screenshots for each step

--- lab3/observer/__SHELL29.java
+++ lab3/observer/__SHELL29.java
@@ -1,5 +1,5 @@
 package observer;
-public class __SHELL27 extends bluej.runtime.Shell {
+public class __SHELL29 extends bluej.runtime.Shell {
 public static void run() throws Throwable {
 final bluej.runtime.BJMap __bluej_runtime_scope = getScope("/Users/yijunzhang/Documents/GitHub/cmpe202/lab3/observer");
 final observer.ObserverTest observerTest = (observer.ObserverTest)__bluej_runtime_scope.get("observerTest");
